fb79c57e60a0fba218c3b0cb6a329509f9ed28aa
Caio Patriota <caiopatriotabarros@gmail.com>
2025-04-24 01:23:53 -0300

    first level resolution

--- anwers/lvl_1/level_1.py
+++ anwers/lvl_1/level_1.py
@@ -9,3 +9,8 @@
 # Hints:
 # Consider use range(#begin, #end) method
 
+numbers = []
+for n in range(2000, 3200):
+    if (n % 7 == 0) and (n % 5 != 0) :
+        numbers.append(str(n))
+print(",".join(numbers))
